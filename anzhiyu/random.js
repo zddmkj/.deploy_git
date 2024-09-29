d112cb0330db8c3738e2265490dda764ec724840
zddmkj <1007086605@qq.com>
2024-09-29 10:59:30 +0800

    Site updated: 2024-09-29 10:59:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/27/hello-world/"];function toRandomPost(){
+var posts=["2024/09/29/Markdown语言介绍/","2024/09/27/Visual Studio Code 介绍/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
